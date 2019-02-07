0e164c60e61283d2a4c8a250f8cdb739301b6d3f
Peter <pitrens.studio@gmail.com>
2019-02-07 10:43:42 +0800

    setup config for google auth

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there, this is an upgrade version' })
-});
+passport.use(new GoogleStrategy())
 
 app.listen(PORT, () => {
   console.log('App is listening')
