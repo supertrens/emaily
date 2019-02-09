e132279fd16e8d3de8c1d35ca666674b928183bd
Peter <pitrens.studio@gmail.com>
2019-02-09 11:42:29 +0800

    improvement: serialize user after beign set in db

--- authentication/passport.js
+++ authentication/passport.js
@@ -4,22 +4,46 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./../config/key');
 const User = require('./../models/User');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
 passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-  },
-    (accessToken, refreshToken, profile, done) => {
-      const newUser = new User({
-        googleId: profile.id,
-        familyName: profile.name.familyName,
-        givenName: profile.name.givenName
-      });
+  }, validateGoogleProfile));
 
-      newUser.save().then(user => {
-        console.log(user);
-      }).catch(e => console.log(e))
+async function validateGoogleProfile(accessToken, refreshToken, profile, done) {
+  try {
+    const userInDB = await User.findOne({ googleId: profile.id });
+
+    if (userInDB) {
+      done(null, userInDB);
+    } else {
+      const user = createUser(profile);
+      if (user) {
+        done(null, user);
+      }
     }
-  )
-);
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const createUser = async (profile) => {
+  const newUser = new User({
+    googleId: profile.id,
+    familyName: profile.name.familyName,
+    givenName: profile.name.givenName
+  });
+
+  try {
+    const user = await newUser.save();
+
+    return user;
+  } catch (err) {
+    console.log(err)
+  }
+}
